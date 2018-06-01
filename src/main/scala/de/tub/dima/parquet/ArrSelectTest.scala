1b3b74cfc70e494e1456558fc822f58d271cb0d5
dieutth <dieufunny@gmail.com>
2018-06-01 12:42:49 -0400

    Join and Map without Cartesian

--- src/main/scala/de/tub/dima/parquet/ArrSelectTest.scala
+++
@@ -1,7 +0,0 @@
-package de.tub.dima.parquet
-
-object ArrSelectTest {
-  def main(args: Array[String]): Unit = {
-
-  }
-}
--- src/main/scala/de/tub/dima/parquet/MapArrArrSingleMatrixNoCartesian.scala
+++ src/main/scala/de/tub/dima/parquet/MapArrArrSingleMatrixNoCartesian.scala
@@ -1,22 +1,24 @@
 package de.tub.dima.parquet
 
 import com.google.common.hash.Hashing
+import org.apache.spark.SparkContext
+import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.rdd.RDD
-
-import scala.collection.mutable.ListBuffer
 
 /*
   @author: dieutth
  */
 
-object ArrArrGMap {
+object MapArrArrSingleMatrixNoCartesian {
 
   def apply(
+           sc: SparkContext,
              ref: RDD[((Int, Long, Long, Short), (Array[Long],Array[Array[Double]]))],
              exp: RDD[((Int, Long, Long, Short), (Array[Long],Array[Array[Double]]))],
              bin: Int
            ): RDD[((Int, Long, Long, Short), (Array[Long], Array[Array[Double]]))] = {
 
+    val allExpIds = sc.broadcast(exp.flatMap(_._2._1).distinct().collect())
     val binnedRef = ref.flatMap{
       x =>
         val startbin = x._1._2/bin
@@ -35,12 +37,13 @@ object ArrArrGMap {
           yield ((x._1._1, i), (x._1._2, x._1._3, x._1._4, x._2._1, x._2._2))
     }
 
-    execute(binnedRef, binnedExp, bin)
+    execute(binnedRef, binnedExp, bin, allExpIds)
   }
 
   private def execute(ref: RDD[((Int,Long),(Long,Long,Short,Array[Long], Array[Array[Double]]))],
                       exp: RDD[((Int,Long),(Long,Long,Short,Array[Long], Array[Array[Double]]))],
-                      bin: Int):
+                      bin: Int,
+                      allExpIds: Broadcast[Array[Long]]):
   RDD[((Int, Long, Long, Short), (Array[Long], Array[Array[Double]]))] = {
 
     val RefJoinedExp = ref.cogroup(exp)
@@ -51,27 +54,33 @@ object ArrArrGMap {
           val res =
             r.flatMap { refRecord =>
               val key = (x._1._1, refRecord._1, refRecord._2, refRecord._3)
-              e.flatMap {
-                expRecord =>
-                  if (
-                  //region intersect
-                    (refRecord._1 < expRecord._2 && expRecord._1 < refRecord._2)
-                      //strand equal or at least one of the strand is non-determine
-                      && (refRecord._3.equals('*') || expRecord._3.equals('*') || refRecord._3.equals(expRecord._3))
-                      //produce result only when bin contains the start of either REF region or EXP region
-                      && (x._1._2 == refRecord._1 / bin || x._1._2 == expRecord._1 / bin)
-                  ) {
+              if (e.isEmpty) {
+                None
+                val ids = (refRecord._4, allExpIds.value)
+                Some((key, (ids, refRecord._5, Array.fill(allExpIds.value.length)(0))))
+              }
+              else {
+                e.flatMap {
+                  expRecord =>
                     val ids = (refRecord._4, expRecord._4)
-//                    Some((key, (ids, refRecord._5, Array[Int]())))
-                    Some((key, (ids, refRecord._5, Array.fill(expRecord._4.length)(1))))
-                  }
-                  else {
-                    None
-                  }
+                    if (
+                    //region intersect
+                      (refRecord._1 < expRecord._2 && expRecord._1 < refRecord._2)
+                        //strand equal or at least one of the strand is non-determine
+                        && (refRecord._3.equals('*') || expRecord._3.equals('*') || refRecord._3.equals(expRecord._3))
+                        //produce result only when bin contains the start of either REF region or EXP region
+                        && (x._1._2 == refRecord._1 / bin || x._1._2 == expRecord._1 / bin)
+                    ) {
 
+                      //                    Some((key, (ids, refRecord._5, Array[Int]())))
+                      Some((key, (ids, refRecord._5, Array.fill(expRecord._4.length)(1))))
+                    }
+                    else {
+                      Some((key, (ids, refRecord._5, Array.fill(expRecord._4.length)(1))))
+                    }
+                }
               }
             }
-
 
           res
       }
@@ -88,12 +97,15 @@ object ArrArrGMap {
       }
       .mapValues {
         x =>
+          val allExpIdsWithCount = allExpIds.value zip Array.fill(allExpIds.value.length)(0)
+          val (expIds, counts) = (allExpIdsWithCount ++ (x._1._2 zip x._3) groupBy(x=>x._1) mapValues(_.map(_._2).sum)).toSeq.unzip
+
           val ids = for (refId <- x._1._1;
-                         expId <- x._1._2
+                         expId <- expIds
           ) yield Hashing.md5().newHasher().putLong(refId).putLong(expId).hash().asLong
 
           val features = for (refFeature <- x._2;
-                              expFeature <- x._3)
+                              expFeature <- counts)
             yield refFeature :+ expFeature.toDouble
           (ids, features)
       }
