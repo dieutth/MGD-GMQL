dc2132ce33c2856eea3d8ad95552afb3a1d6e389
dieutth <dieufunny@gmail.com>
2018-06-01 12:51:44 -0400

    read data using unified Loaders

--- src/main/scala/de/tub/dima/parquet/ArrArrLeftJoinTest.scala
+++ src/main/scala/de/tub/dima/parquet/ArrArrLeftJoinTest.scala
@@ -1,9 +1,10 @@
 package de.tub.dima.parquet
 
-import de.tub.dima.MGD_GMQLSparkExecutor
-import it.polimi.genomics.GMQLServer.GmqlServer
+import com.google.common.hash.Hashing
+import de.tub.dima.loaders.{BedParser, CustomParser, Loaders}
 import it.polimi.genomics.core.DataStructures.JoinParametersRD.{DistLess, RegionBuilder}
-import it.polimi.genomics.core.GMQLSchemaFormat
+import it.polimi.genomics.core.GDouble
+import org.apache.spark.sql.functions.{input_file_name, lit}
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
@@ -17,70 +18,119 @@ object ArrArrLeftJoinTest {
       .set("spark.executor.heartbeatInterval","300s")
       .set("spark.eventLog.enabled", "true")
       .set("spark.eventLog.dir","/tmp/spark-events")
+      .set("spark.default.parallelism", "2")
 
     val spark =  SparkSession.builder().config(conf).getOrCreate()
     val sc = spark.sparkContext
 
-    val startTime = System.currentTimeMillis()
     val bin = 10000
     val gDist = DistLess(70000)
 
     val (ds1Path, ds2Path, loop) = if (args.length == 3) (args(0), args(1), args(2))
-    else ("/home/dieutth/data/gmql/parquet/TAD_Aidens_id", "/home/dieutth/data/gmql/parquet/TAD_Aidens_id", "3")
+//    else ("/home/dieutth/data/gmql/parquet/TAD_Aidens_id", "/home/dieutth/data/gmql/parquet/TAD_Aidens_id", "3")
+//    else ("/home/dieutth/data/gmql/uncompressed/TADs_Aiden/", "/home/dieutth/data/gmql/uncompressed/TADs_Aiden/", "3")
+    else ("/home/dieutth/data/gmql/uncompressed/TADs_Aiden_working_medium/", "/home/dieutth/data/gmql/uncompressed/TADs_Aiden_working_medium/", "3")
+//    else ("/home/dieutth/data/gmql/uncompressed/tmp/ref/", "/home/dieutth/data/gmql/uncompressed/tmp/exp/", "3")
 
-//    val exp: RDD[((Int, Long),(Long, Long, Short, Array[Long], Array[Array[Double]]))]
-    val exp = spark.read.parquet(ds1Path)
-      .rdd
-      //group by coordinate
-      .groupBy { x =>
-      val chrStr = x.getString(1).substring(3)
-      val chr: Int = try {
-        chrStr.toInt
-      } catch {
-        case e: Exception => 23
+
+    val startTime = System.currentTimeMillis()
+    val ref = Loaders.forPath(sc, ds1Path).LoadRegionsCombineFiles(new CustomParser().setSchema(ds1Path).region_parser)
+      .groupBy{
+        x =>
+          val y = x._1.chrom.substring(3)
+          val chr: Int = try {
+            y.toInt
+          } catch {
+            case e: Exception => if (y == "X") 23 else 24
+          }
+          (chr, x._1.start, x._1.stop, x._1.strand.toShort)
       }
-      (chr, x.getLong(2), x.getLong(3), x.getShort(4))
-    }
-      //convert each group of item with the same coordinate to an array of coordinate, with list of ids
-      .map(x => (x._1, (x._2.map(r => r.getLong(0)).toArray, x._2.map(_ => Array[Double]()).toArray)))
+      .map(x=>
+        (x._1, x._2.map(y => (y._1.id, y._2.flatMap(_ match {case GDouble(t)=> Some(t)
+        case _=> None}))).toArray.unzip)
 
+      )
 
-
-//    val ref: RDD[((Int, Long),(Long, Long, Short, Array[Long], Array[Array[Double]]))]
-    val ref = spark.read.parquet(ds2Path)
-      .rdd
-      //group by coordinate
-      .groupBy { x =>
-      val chrStr = x.getString(1).substring(3)
-      val chr: Int = try {
-        chrStr.toInt
-      } catch {
-        case e: Exception => 23
+    val exp = Loaders.forPath(sc, ds2Path).LoadRegionsCombineFiles(new CustomParser().setSchema(ds2Path).region_parser)
+      .groupBy{
+        x =>
+          val y = x._1.chrom.substring(3)
+          val chr: Int = try {
+            y.toInt
+          } catch {
+            case e: Exception => if (y == "X") 23 else 24
+          }
+          (chr, x._1.start, x._1.stop, x._1.strand.toShort)
       }
-      (chr, x.getLong(2), x.getLong(3), x.getShort(4))
+      .map(x=>
+        (x._1, x._2.map(y => (y._1.id, y._2.flatMap(_ match {case GDouble(t)=> Some(t)
+        case _=> None}))).toArray.unzip)
+
+      )
+
+    testArrMap(1)
+//    testArrJoin(2)
+//    testArrJoinNoCartesian(2)
+    def testArrMap(loop: Int) = {
+      var reduced = MapArrArrSingleMatrixNoCartesian(sc, ref, exp, bin)
+      for (i <- Range(1, loop))
+        reduced = MapArrArrSingleMatrixNoCartesian(sc, ref, reduced, bin)
+      val r = reduced
+        .flatMap {
+          x =>
+            val k = ("chr"+x._1._1, x._1._2, x._1._3, x._1._4.toChar)
+            for (item <- (x._2._1 zip x._2._2))
+              yield (k, item._1, item._2.mkString(","))
+        }
+        .saveAsTextFile("/home/dieutth/testparquet/mapNoCartesian/")
+      println("Execution time for Map:" + (System.currentTimeMillis() - startTime) / 1000)
+
     }
-      //convert each group of item with the same coordinate to an array of coordinate, with list of ids
-      .map(x => (x._1, (x._2.map(r => r.getLong(0)).toArray, x._2.map(_ => Array[Double]()).toArray)))
 
-    var reduced = ArrArrJoin(ref, exp, bin, RegionBuilder.LEFT, Some(gDist), None)
-    for (i <- Range(1,loop.toInt))
-      reduced = ArrArrJoin(ref, reduced, bin, RegionBuilder.LEFT, Some(gDist), None)
-//    val tmp1 = ArrArrJoin(ref, exp, bin, RegionBuilder.LEFT, Some(gDist), None)
-//    val tmp =  ArrArrJoin(exp, tmp1, bin, RegionBuilder.LEFT, Some(gDist), None)
-//    val reduced =
-//     val count = reduced
-//                  .map{
-//                    x =>
-//                      x._2._1.size
-////                      val k = x._1
-////                      for (item <- (x._2._1 zip x._2._2))
-////                        yield (k, item._1, item._2.mkString("[", ",", "]"))
-//                  }
-//    reduced.saveAsTextFile("/home/dieutth/testparquet/join/")
-    println(reduced.count())
-    println ("Execution time when running without custom-partitioner:" + (System.currentTimeMillis() - startTime )/1000)
+    def testArrJoin(loop: Int) = {
+      var reduced = ArrArrJoin(ref, exp, bin, RegionBuilder.LEFT, Some(gDist), None)
+      for (i <- Range(1, loop))
+//        reduced = ArrArrJoin(ref, reduced, bin, RegionBuilder.LEFT, Some(gDist), None)
+        reduced = ArrArrJoin(reduced, ref, bin, RegionBuilder.LEFT, Some(gDist), None)
+      reduced
+//        .flatMap {
+//          x =>
+//            val k = x._1
+//            for (item <- (x._2._1 zip x._2._2))
+//              yield (k, item._1, item._2.mkString(","))
+//        }
+        .saveAsTextFile("/home/dieutth/testparquet/joinNormal/")
+      println("Execution time for normal join :" + (System.currentTimeMillis() - startTime) / 1000)
+    }
 
+    def testArrJoinNoCartesian(loop: Int) = {
+          var reduced = ArrArrJoin_NoCartesian(ref, exp, bin, RegionBuilder.LEFT, Some(gDist), None)
+      for (i <- Range(1, loop))
+//            reduced = ArrArrJoin_NoCartesian(ref, reduced, bin, RegionBuilder.LEFT, Some(gDist), None)
+            reduced = ArrArrJoin_NoCartesian(reduced, ref, bin, RegionBuilder.LEFT, Some(gDist), None)
+      reduced
+        .saveAsTextFile("/home/dieutth/testparquet/joinNoCartesian/")
+      println("Execution time when running on Join No cartesian:" + (System.currentTimeMillis() - startTime) / 1000)
+    }
   }
+
+//  private def readDS(spark: SparkSession, path: String): Map[String, Long] ={
+//    val allFiles = spark.read.text(path)
+//                  .select(input_file_name)
+//                    .flatMap{
+//                      x =>
+//                        val filename = x.getString(0)
+//                        if (filename.endsWith("schema") || filename.endsWith("meta"))
+//                          None
+//                        else{
+//                          val fn = filename.substring(0, filename.lastIndexOf("."))
+//                          val hashKey = Hashing.md5().newHasher().putString(fn.replaceAll("/",""),java.nio.charset.StandardCharsets.UTF_8).hash().asLong()
+//                         Some(filename, hashKey)
+//                        }
+//                    }.collect().toMap
+//    allFiles
+//
+//  }
 
 }
 
--- src/main/scala/de/tub/dima/test.scala
+++ src/main/scala/de/tub/dima/test.scala
@@ -1,6 +1,6 @@
 package de.tub.dima
 
-import loaders.CustomParser
+import loaders.{BedParser, CustomParser, Loaders}
 import it.polimi.genomics.GMQLServer.GmqlServer
 import it.polimi.genomics.core.DataStructures.JoinParametersRD.{DistLess, JoinQuadruple, RegionBuilder}
 import it.polimi.genomics.core._
@@ -15,36 +15,51 @@ object test {
       .set("spark.executor.heartbeatInterval","300s")
       .set("spark.eventLog.enabled", "true")
       .set("spark.eventLog.dir","/tmp/spark-events")
+      .set("spark.default.parallelism", "2")
     val sc:SparkContext =new SparkContext(conf)
 
     val server = new GmqlServer(new MGD_GMQLSparkExecutor(sc=sc,outputFormat = GMQLSchemaFormat.TAB))
 
-    val dsFilePath1 = "/home/dieutth/data/gmql/uncompressed/TADs_Aiden/" // /home/dieutth/share/data/Example_Dataset_1/files"
-    val dsFilePath2 = "/home/dieutth/data/gmql/uncompressed/TADs_Aiden/" // /home/dieutth/share/data/Example_Dataset_2/files"
+//    val dsFilePath1 = "/home/dieutth/data/gmql/uncompressed/TADs_Aiden_working_medium/"///home/dieutth/data/gmql/uncompressed/TADs_Aiden/" // /home/dieutth/share/data/Example_Dataset_1/files"
+//    val dsFilePath2 = "/home/dieutth/data/gmql/uncompressed/TADs_Aiden_working_medium/"///home/dieutth/data/gmql/uncompressed/TADs_Aiden/" // /home/dieutth/share/data/Example_Dataset_2/files"
 
-    val timestamp = System.currentTimeMillis()
-    val ds1 = server READ(dsFilePath1 ) USING (new CustomParser().setSchema(dsFilePath1))
-    val ds2 = server READ(dsFilePath2 ) USING (new CustomParser().setSchema(dsFilePath2))
+    val (dsFilePath1, dsFilePath2, loop) = if (args.length == 3) (args(0), args(1), args(2))
+    //    else ("/home/dieutth/data/gmql/parquet/TAD_Aidens_id", "/home/dieutth/data/gmql/parquet/TAD_Aidens_id", "3")
+    //    else ("/home/dieutth/data/gmql/uncompressed/TADs_Aiden/", "/home/dieutth/data/gmql/uncompressed/TADs_Aiden/", "3")
+        else ("/home/dieutth/data/gmql/uncompressed/TADs_Aiden_working_medium/", "/home/dieutth/data/gmql/uncompressed/TADs_Aiden_working_medium/", "3")
+//    else ("/home/dieutth/data/gmql/uncompressed/tmp/ref/", "/home/dieutth/data/gmql/uncompressed/tmp/exp/", "3")
 
-/*-------------------------Testing for Map operator-----------------------------------------------------*/
-//    val map = ds1.MAP(None,List(),ds2)
-//    server setOutputPath("/home/dieutth/result/") MATERIALIZE(map)
-//    server.run()
-//    println ("Execution time of current system join: ",(System.currentTimeMillis() - timestamp )/1000)
+    testMap(0)
+    def testMap(loop: Int): Unit = {
+      val timestamp = System.currentTimeMillis()
+      val ds1 = server READ(dsFilePath1 ) USING (new CustomParser().setSchema(dsFilePath1))
+      val ds2 = server READ(dsFilePath2 ) USING (new CustomParser().setSchema(dsFilePath2))
+
+      var map = ds1.MAP(None,List(),ds2)
+      for (i <- Range(0, loop))
+        map = ds1.MAP(None,List(),map)
+
+      server setOutputPath("/home/dieutth/result/current_system_map/") MATERIALIZE(map)
+      server.run()
+      println ("Execution time of current system map: " + (System.currentTimeMillis() - timestamp )/1000 + " loop = " + loop)
+    }
 
 
-    /*-------------------------Testing for Join operator-----------------------------------------------------*/
-        var join = ds1.JOIN(None, List(new JoinQuadruple(Some(DistLess(70000)))), RegionBuilder.LEFT, ds2)
+    def testJoin: Unit = {
+      val timestamp = System.currentTimeMillis()
+      val ds1 = server READ(dsFilePath1 ) USING (new CustomParser().setSchema(dsFilePath1))
+      val ds2 = server READ(dsFilePath2 ) USING (new CustomParser().setSchema(dsFilePath2))
 
-//        for (i <- Range(1, 10))
-          join = join.JOIN(None, List(new JoinQuadruple(Some(DistLess(70000)))), RegionBuilder.LEFT, ds2)
+      var join = ds1.JOIN(None, List(new JoinQuadruple(Some(DistLess(70000)))), RegionBuilder.LEFT, ds2)
 
-        server setOutputPath("/home/dieutth/result/tmpp4/") MATERIALIZE(join)
-        server.run()
-        println ("Execution time of current system join: ",(System.currentTimeMillis() - timestamp )/1000)
+      //        for (i <- Range(1, 10))
+     // join = ds2.JOIN(None, List(new JoinQuadruple(Some(DistLess(70000)))), RegionBuilder.LEFT, join)
 
+      server setOutputPath("/home/dieutth/result/current_system_join/") MATERIALIZE(join)
+      server.run()
+      println ("Execution time of current system join: ",(System.currentTimeMillis() - timestamp )/1000)
 
+    }
 
   }
-
 }
