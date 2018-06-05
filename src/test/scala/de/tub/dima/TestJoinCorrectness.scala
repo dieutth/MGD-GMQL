ff8e69106b87fc5ce9913d7e063ec0961b012b32
dieutth <dieufunny@gmail.com>
2018-06-05 05:31:09 -0400

    set up 2/5 test cases for Join

--- src/test/scala/de/tub/dima/TestJoinCorrectness.scala
+++ src/test/scala/de/tub/dima/TestJoinCorrectness.scala
@@ -1,6 +1,6 @@
 package de.tub.dima
 
-import de.tub.dima.operators.join.ArrArrJoin
+import de.tub.dima.operators.join.{ArrArrJoin, ArrArrJoin_Multimatrix, ArrArrJoin_NoCartesian}
 import de.tub.dima.operators.map.Map_ArrArr_NoCartesian
 import it.polimi.genomics.core.DataStructures.JoinParametersRD.{DistLess, RegionBuilder}
 import it.polimi.genomics.core.DataTypes.GRECORD
@@ -16,127 +16,199 @@ import it.polimi.genomics.core.{GDouble, GRecordKey, GValue}
   *
   */
 class TestJoinCorrectness extends TestBase {
-    val distLess = Some(DistLess(0))
-    val distGreater = None
+  val distLess = Some(DistLess(0))
+  val distGreater = None
 
-    val expectedResultNoRegionDup = Array(
+    /**
+    *
+    * ================================================NO_REGION_DUP ================================================
+    *
+    ** REF:
+    * S0, chr1	1	20	*	0.2	0.3
+    * S0, chr1	1	3	*	0.2	0.7
+    * S1, chr1	1	3	*	0.7	0.2
+    * S1, chr1	5	94	*	0.2	0.2
+    *
+    ** EXP:
+    * S0, chr1	2	5	*	0.2	0.3
+    * S1, chr1	5	100	*	0.7	9.9
+    * S1, chr1	4	70	*	0.7	0.2
+    * S2, chr1	50	101	*	0.2	0.3
+    */
+  val expectedResultNoRegionDup = Array(
+    (GRecordKey(s10, "chr1", 1, 3, '*'), Array[GValue](GDouble(0.7), GDouble(0.2), GDouble(0.2), GDouble(0.3))),
+    (GRecordKey(s00, "chr1", 1, 3, '*'), Array[GValue](GDouble(0.2), GDouble(0.7), GDouble(0.2), GDouble(0.3))),
 
-    )
-    test("Join Arr-Arr: No region-duplication in each sample in both dataset"){
-      val refFilePath = "/home/dieutth/git/MGD-GMQL/src/test/resources/no_region_duplicate_in_both/ref/"
-      val expFilePath = "/home/dieutth/git/MGD-GMQL/src/test/resources/no_region_duplicate_in_both/exp/"
-      val ref = loadDataset(refFilePath).transformToSingleMatrix()
-      val exp = loadDataset(expFilePath).transformToSingleMatrix()
+    (GRecordKey(s11, "chr1", 5, 94, '*'), Array[GValue](GDouble(0.2), GDouble(0.2), GDouble(0.7), GDouble(9.9))),
+    (GRecordKey(s11, "chr1", 5, 94, '*'), Array[GValue](GDouble(0.2), GDouble(0.2), GDouble(0.7), GDouble(0.2))),
+    (GRecordKey(s12, "chr1", 5, 94, '*'), Array[GValue](GDouble(0.2), GDouble(0.2), GDouble(0.2), GDouble(0.3))),
 
-      val expectedResult = Array(
-        (GRecordKey(s10, "chr1", 1, 3, '*'), Array[GValue](GDouble(0.7), GDouble(0.2), GDouble(0.2), GDouble(0.3))),
-        (GRecordKey(s00, "chr1", 1, 3, '*'), Array[GValue](GDouble(0.2), GDouble(0.7), GDouble(0.2), GDouble(0.3))),
-
-        (GRecordKey(s11, "chr1", 5, 94, '*'), Array[GValue](GDouble(0.2), GDouble(0.2), GDouble(0.7), GDouble(9.9))),
-        (GRecordKey(s11, "chr1", 5, 94, '*'), Array[GValue](GDouble(0.2), GDouble(0.2), GDouble(0.7), GDouble(0.2))),
-        (GRecordKey(s12, "chr1", 5, 94, '*'), Array[GValue](GDouble(0.2), GDouble(0.2), GDouble(0.2), GDouble(0.3))),
-
-        (GRecordKey(s00, "chr1", 1, 20, '*'), Array[GValue](GDouble(0.2), GDouble(0.3), GDouble(0.2), GDouble(0.3))),
-        (GRecordKey(s01, "chr1", 1, 20, '*'), Array[GValue](GDouble(0.2), GDouble(0.3), GDouble(0.7), GDouble(9.9))),
-        (GRecordKey(s01, "chr1", 1, 20, '*'), Array[GValue](GDouble(0.2), GDouble(0.3), GDouble(0.7), GDouble(0.2)))
-      ).map(Rep(_)).toSet
-
-
-      val actualResult = ArrArrJoin(ref, exp, bin, RegionBuilder.LEFT, distLess, distGreater)
-        .transformToRow().collect().map(Rep(_)).toSet
-
-      assert(expectedResult.subsetOf(actualResult) && actualResult.subsetOf(expectedResult))
-
-    }
-
-
-  test("JOIN Arr-Arr: Region dup in REF only"){
-    val refFilePath = "/home/dieutth/git/MGD-GMQL/src/test/resources/region_dup_in_ref_only/ref/"
-    val expFilePath = "/home/dieutth/git/MGD-GMQL/src/test/resources/region_dup_in_ref_only/exp/"
-    val ref = loadDataset(refFilePath).transformToSingleMatrix()
-    val exp = loadDataset(expFilePath).transformToSingleMatrix()
-
-    val expectedResult = Array(
-      (GRecordKey(s10, "chr1", 1, 3, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(1), GDouble(1))),
-      (GRecordKey(s11, "chr1", 1, 3, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(1), GDouble(1))),
-      (GRecordKey(s11, "chr1", 1, 3, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(2), GDouble(2))),
-
-      (GRecordKey(s10, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(1), GDouble(1))),
-      (GRecordKey(s11, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(1), GDouble(1))),
-      (GRecordKey(s11, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(2), GDouble(2))),
+    (GRecordKey(s00, "chr1", 1, 20, '*'), Array[GValue](GDouble(0.2), GDouble(0.3), GDouble(0.2), GDouble(0.3))),
+    (GRecordKey(s01, "chr1", 1, 20, '*'), Array[GValue](GDouble(0.2), GDouble(0.3), GDouble(0.7), GDouble(9.9))),
+    (GRecordKey(s01, "chr1", 1, 20, '*'), Array[GValue](GDouble(0.2), GDouble(0.3), GDouble(0.7), GDouble(0.2)))
+  ).map(Rep(_)).toSet
 
 
-      (GRecordKey(s00, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(1), GDouble(1))),
-      (GRecordKey(s01, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(1), GDouble(1))),
-      (GRecordKey(s01, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(2), GDouble(2))),
+//  ignore("Join Arr-Arr-Normal: No region-duplication in BOTH") {
+      test("Join Arr-Arr-Normal: No region-duplication in BOTH"){
+    val ref = loadDataset(refFilePath_NoRegionDup).transformToSingleMatrix()
+    val exp = loadDataset(expFilePath_NoRegionDup).transformToSingleMatrix()
+    val actualResult = ArrArrJoin(ref, exp, bin, RegionBuilder.LEFT, distLess, distGreater)
+      .transformToRow().collect().map(Rep(_)).toSet
+    actualResult.foreach(println)
+    assert(expectedResultNoRegionDup.subsetOf(actualResult) && actualResult.subsetOf(expectedResultNoRegionDup))
+  }
 
-      (GRecordKey(s00, "chr1", 1, 7, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(1), GDouble(1))),
-      (GRecordKey(s01, "chr1", 1, 7, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(1), GDouble(1))),
-      (GRecordKey(s01, "chr1", 1, 7, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(2), GDouble(2)))
-    ).map(Rep(_)).toSet
+      test("Join Arr-Arr-NoCartesian: No region-duplication in BOTH"){
+//  ignore("Join Arr-Arr-NoCartesian: No region-duplication in BOTH") {
+    val ref = loadDataset(refFilePath_NoRegionDup).transformToSingleMatrix()
+    val exp = loadDataset(expFilePath_NoRegionDup).transformToSingleMatrix()
+    val actualResult = ArrArrJoin_NoCartesian(ref, exp, bin, RegionBuilder.LEFT, distLess, distGreater)
+      .transformToRow().collect().map(Rep(_)).toSet
 
+    assert(expectedResultNoRegionDup.subsetOf(actualResult) && actualResult.subsetOf(expectedResultNoRegionDup))
+  }
+
+
+  test("Join Arr-Arr-Multimatrix: No region-duplication in BOTH") {
+    val refMulti = loadDataset(refFilePath_NoRegionDup).transformToMultiMatrix()
+    val exp = loadDataset(expFilePath_NoRegionDup).transformToSingleMatrix()
+    val actualResult = ArrArrJoin_Multimatrix(refMulti, exp, bin, RegionBuilder.LEFT, distLess, distGreater)
+      .transformToRow().collect().map(Rep(_)).toSet
+//    actualResult.foreach(println)
+
+    assert(expectedResultNoRegionDup.subsetOf(actualResult) && actualResult.subsetOf(expectedResultNoRegionDup))
+  }
+
+
+  /**
+    *
+    * ================================================REGION_DUP_IN_REF_ONLY ================================================
+    *
+    * * REF:
+    *
+    * * EXP:
+    *
+    */
+
+  val expectedResultRegionDupInREF = Array(
+    (GRecordKey(s10, "chr1", 1, 3, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(1), GDouble(1))),
+    (GRecordKey(s11, "chr1", 1, 3, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(1), GDouble(1))),
+    (GRecordKey(s11, "chr1", 1, 3, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(2), GDouble(2))),
+
+    (GRecordKey(s10, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(1), GDouble(1))),
+    (GRecordKey(s11, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(1), GDouble(1))),
+    (GRecordKey(s11, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(2), GDouble(2))),
+
+
+    (GRecordKey(s00, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(1), GDouble(1))),
+    (GRecordKey(s01, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(1), GDouble(1))),
+    (GRecordKey(s01, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(2), GDouble(2))),
+
+    (GRecordKey(s00, "chr1", 1, 7, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(1), GDouble(1))),
+    (GRecordKey(s01, "chr1", 1, 7, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(1), GDouble(1))),
+    (GRecordKey(s01, "chr1", 1, 7, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(2), GDouble(2)))
+  ).map(Rep(_)).toSet
+
+    test("JOIN Arr-Arr-Normal: Region dup in REF only"){
+//  ignore("JOIN Arr-Arr-Normal: Region dup in REF only") {
+    val ref = loadDataset(refFilePath_RegionDupInREF).transformToSingleMatrix()
+    val exp = loadDataset(expFilePath_RegionDupInREF).transformToSingleMatrix()
     val actualResult = ArrArrJoin(ref, exp, bin, RegionBuilder.LEFT, distLess, distGreater).transformToRow().collect().map(Rep(_)).toSet
-    assert(expectedResult.subsetOf(actualResult) && actualResult.subsetOf(expectedResult))
+
+    assert(expectedResultRegionDupInREF.subsetOf(actualResult) && actualResult.subsetOf(expectedResultRegionDupInREF))
+  }
+
+//  ignore("JOIN Arr-Arr-NoCartesian: Region dup in REF only") {
+      test("JOIN Arr-Arr-NoCartesian: Region dup in REF only"){
+    val ref = loadDataset(refFilePath_RegionDupInREF).transformToSingleMatrix()
+    val exp = loadDataset(expFilePath_RegionDupInREF).transformToSingleMatrix()
+    val actualResult = ArrArrJoin_NoCartesian(ref, exp, bin, RegionBuilder.LEFT, distLess, distGreater).transformToRow().collect().map(Rep(_)).toSet
+
+    assert(expectedResultRegionDupInREF.subsetOf(actualResult) && actualResult.subsetOf(expectedResultRegionDupInREF))
+  }
+
+  test("JOIN Arr-Arr-MultiMatrix: Region dup in REF only") {
+    val refMulti = loadDataset(refFilePath_RegionDupInREF).transformToMultiMatrix()
+    val exp = loadDataset(expFilePath_RegionDupInREF).transformToSingleMatrix()
+    val actualResult = ArrArrJoin_Multimatrix(refMulti, exp, bin, RegionBuilder.LEFT, distLess, distGreater).transformToRow().collect().map(Rep(_)).toSet
+//    actualResult.foreach(println)
+
+    assert(expectedResultRegionDupInREF.subsetOf(actualResult) && actualResult.subsetOf(expectedResultRegionDupInREF))
   }
 
 
-  ignore("Arr-Arr: Region dup in EXP only"){
-//  test("Arr-Arr: Region dup in EXP only"){
-    val refFilePath = "/home/dieutth/git/MGD-GMQL/src/test/resources/region_dup_in_exp_only/ref/"
-    val expFilePath = "/home/dieutth/git/MGD-GMQL/src/test/resources/region_dup_in_exp_only/exp/"
-    val ref = loadDataset(refFilePath).transformToSingleMatrix()
-    val exp = loadDataset(expFilePath).transformToSingleMatrix()
+  /**
+    *
+    * ================================================REGION_DUP_IN_EXP_ONLY ================================================
+    *
+    * * REF:
+    * S0, chr1	1	7	*	1	1
+    * S0, chr1	1	3	*	2	2
+    *
+    * * EXP:
+    * S0  chr1  2 6 * 1 1 //intersect with both
+    * S0  chr1  2 6 * 2 2 //intersect with both, dup region
+    * S0  chr1  4 8 * 8 8 //intersect with only one
+    * S0  chr1  4 8 * 9 9 //intersect with only 1, dup region
+    * S0  chr1  8 9 * 9 9 // no-intersect
+    *
+    * S1  chr1  2 5 * 5 5 //intersect with both
+    * S1  chr1  2 6 * 6 6 //intersect with both, dup region with S0
+    * S1  chr1  8 9 8 7 7 //no-intersect
+    * S1  chr1  4 8 * 2 2 //intersect with only one, dup region with S0
+    * S1  chr1  4 9 * 8 8 //intersect with only one, no dup region with S1
+    *
+    */
 
-    val expectedResult = Array(
-      (GRecordKey(s10, "chr1", 1, 3, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(1))),
-      (GRecordKey(s11, "chr1", 1, 3, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(2))),
 
-      (GRecordKey(s10, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(1))),
-      (GRecordKey(s11, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(2))),
+  val expectedResultRegionDupInEXP = Array(
+    (GRecordKey(s10, "chr1", 1, 3, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(1), GDouble(1)))
+//    (GRecordKey(s11, "chr1", 1, 3, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(1), GDouble(1))),
+//    (GRecordKey(s11, "chr1", 1, 3, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(2), GDouble(2))),
+//
+//    (GRecordKey(s10, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(1), GDouble(1))),
+//    (GRecordKey(s11, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(1), GDouble(1))),
+//    (GRecordKey(s11, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(2), GDouble(2))),
+//
+//
+//    (GRecordKey(s00, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(1), GDouble(1))),
+//    (GRecordKey(s01, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(1), GDouble(1))),
+//    (GRecordKey(s01, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(2), GDouble(2))),
+//
+//    (GRecordKey(s00, "chr1", 1, 7, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(1), GDouble(1))),
+//    (GRecordKey(s01, "chr1", 1, 7, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(1), GDouble(1))),
+//    (GRecordKey(s01, "chr1", 1, 7, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(2), GDouble(2)))
+  ).map(Rep(_)).toSet
 
+  ignore("JOIN Arr-Arr-Normal: Region dup in EXP only") {
+//  test("JOIN Arr-Arr-Normal: Region dup in EXP only") {
+    val ref = loadDataset(refFilePath_RegionDupInEXP).transformToSingleMatrix()
+    val exp = loadDataset(expFilePath_RegionDupInEXP).transformToSingleMatrix()
+    val actualResult = ArrArrJoin(ref, exp, bin, RegionBuilder.LEFT, distGreater, distGreater).transformToRow().collect().map(Rep(_)).toSet
 
-      (GRecordKey(s00, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(1))),
-      (GRecordKey(s01, "chr1", 1, 3, '*'), Array[GValue](GDouble(2), GDouble(2), GDouble(2))),
-
-      (GRecordKey(s00, "chr1", 1, 7, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(1))),
-      (GRecordKey(s01, "chr1", 1, 7, '*'), Array[GValue](GDouble(1), GDouble(1), GDouble(2)))
-    ).map(Rep(_)).toSet
-
-    val actualResult = Map_ArrArr_NoCartesian(sc, ref, exp, bin).transformToRow().collect().map(Rep(_)).toSet
-    assert(expectedResult.subsetOf(actualResult) && actualResult.subsetOf(expectedResult))
+    assert(expectedResultRegionDupInEXP.subsetOf(actualResult) && actualResult.subsetOf(expectedResultRegionDupInEXP))
 
   }
 
 
-  ignore("Arr-Arr: Region dup in BOTH dataset"){
-//  test("Arr-Arr: Region dup in BOTH dataset"){
-    val refFilePath = "/home/dieutth/git/MGD-GMQL/src/test/resources/region_dup_in_both/ref/"
-    val expFilePath = "/home/dieutth/git/MGD-GMQL/src/test/resources/region_dup_in_both/exp/"
-    val ref = loadDataset(refFilePath).transformToSingleMatrix()
-    val exp = loadDataset(expFilePath).transformToSingleMatrix()
+  ignore("JOIN Arr-Arr-NoCartesian: Region dup in EXP only") {
+//  test("JOIN Arr-Arr-NoCartesian: Region dup in EXP only") {
+    val ref = loadDataset(refFilePath_RegionDupInEXP).transformToSingleMatrix()
+    val exp = loadDataset(expFilePath_RegionDupInEXP).transformToSingleMatrix()
+    val actualResult = ArrArrJoin_NoCartesian(ref, exp, bin, RegionBuilder.LEFT, distGreater, distGreater).transformToRow().collect().map(Rep(_)).toSet
 
-    val expectedResult = Array[GRECORD](
-    ).map(Rep(_)).toSet
-
-    val actualResult = Map_ArrArr_NoCartesian(sc, ref, exp, bin).transformToRow().collect().map(Rep(_)).toSet
-    assert(expectedResult.subsetOf(actualResult) && actualResult.subsetOf(expectedResult))
+    assert(expectedResultRegionDupInEXP.subsetOf(actualResult) && actualResult.subsetOf(expectedResultRegionDupInEXP))
 
   }
 
-
-
-  ignore("Arr-Arr: Record dup in REF only"){
-//  test("Arr-Arr: Record dup in REF only"){
-    val refFilePath = "/home/dieutth/git/MGD-GMQL/src/test/resources/region_dup_in_ref_only/ref/"
-    val expFilePath = "/home/dieutth/git/MGD-GMQL/src/test/resources/region_dup_in_ref_only/exp/"
-    val ref = loadDataset(refFilePath).transformToSingleMatrix()
-    val exp = loadDataset(expFilePath).transformToSingleMatrix()
-
-    val expectedResult = Array[GRECORD](
-    ).map(Rep(_)).toSet
-
+  ignore("JOIN Arr-Arr-Multimatrix: Region dup in EXP only") {
+//  test("JOIN Arr-Arr-Multimatrix: Region dup in EXP only") {
+    val ref = loadDataset(refFilePath_RegionDupInEXP).transformToSingleMatrix()
+    val exp = loadDataset(expFilePath_RegionDupInEXP).transformToSingleMatrix()
     val actualResult = Map_ArrArr_NoCartesian(sc, ref, exp, bin).transformToRow().collect().map(Rep(_)).toSet
-    assert(expectedResult.subsetOf(actualResult) && actualResult.subsetOf(expectedResult))
+
+    assert(expectedResultRegionDupInEXP.subsetOf(actualResult) && actualResult.subsetOf(expectedResultRegionDupInEXP))
 
   }
 }
