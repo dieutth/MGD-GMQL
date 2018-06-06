c6123061e5840ce40ea1d969f83c44aa431a74b0
dieutth <dieufunny@gmail.com>
2018-06-06 15:29:22 -0400

    add comment to LocalBenchmarking

--- src/main/scala/de/tub/dima/BENCHMARKING/LocalBenchmarking.scala
+++ src/main/scala/de/tub/dima/BENCHMARKING/LocalBenchmarking.scala
@@ -1,9 +1,14 @@
 package de.tub.dima.BENCHMARKING
 
-import it.polimi.genomics.GMQLServer.GmqlServer
-import it.polimi.genomics.core.GMQLSchemaFormat
-import it.polimi.genomics.spark.implementation.GMQLSparkExecutor
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.SparkConf
+
+/**
+  * @author dieutth, 06/06/2018
+  *
+  *  Main method to benchmark operators performance (by measuring execution time) when executing in IDE on small datasets.
+  *  For a fair comparison, when perform a benchmark for a particular experiment, commenting all other benchmarks code,
+  *  and run only the experiment of interest.
+  */
 
 object LocalBenchmarking {
 
@@ -19,6 +24,7 @@ object LocalBenchmarking {
       .set("spark.default.parallelism", "2")
     
 
+    //Various datasets with different characteristics that could be used for benchmarking.
     val (refPath, expPath, loop) =
     //     ("/home/dieutth/data/gmql/parquet/TAD_Aidens_id", "/home/dieutth/data/gmql/parquet/TAD_Aidens_id", "3")
      ("/home/dieutth/data/gmql/uncompressed/TADs_Aiden/", "/home/dieutth/data/gmql/uncompressed/TADs_Aiden/", "3")
@@ -26,11 +32,10 @@ object LocalBenchmarking {
     //     ("/home/dieutth/data/gmql/uncompressed/TADs_Aiden_working_medium/", "/home/dieutth/data/gmql/uncompressed/TADs_Aiden_working_medium/", "3")
     //     ("/home/dieutth/data/gmql/uncompressed/tmp/ref/", "/home/dieutth/data/gmql/uncompressed/tmp/exp/", "3")
     //     ("/home/dieutth/data/gmql/uncompressed/tmp_dupIn1Sample/", "/home/dieutth/data/gmql/uncompressed/tmp_dupIn1Sample/", "3")
-    //    ("/home/dieutth/git/MGD-GMQL/src/test/resources/no_region_duplicate_in_both/ref/",
-    //          "/home/dieutth/git/MGD-GMQL/src/test/resources/no_region_duplicate_in_both/exp/" ,"3")
 
 
-//    val testCurrentMap = Benchmark(conf, refPath, expPath, "/home/dieutth/data/gmql/result/current_system_map").benchmarkCurrentSystem_Map(0)
+
+    val testCurrentMap = Benchmark(conf, refPath, expPath, "/home/dieutth/data/gmql/result/current_system_map").benchmarkCurrentSystem_Map(0)
 //    val testCurrentJoin = Benchmark(conf, refPath, expPath, "/home/dieutth/data/gmql/result/current_system_join").benchmarkCurrentSystem_Join(0)
 //    val testArrArrJoin = Benchmark(conf, refPath, expPath, "/home/dieutth/data/gmql/result/joinNormal").benchmarkArrArrJoin(0)
 //    val testArrArrJoinNoCartesian = Benchmark(conf, refPath, expPath, "/home/dieutth/data/gmql/result/joinNoCartesian").benchmarkArrArrJoinNoCartesian(0)
